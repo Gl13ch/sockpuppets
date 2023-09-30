6440f8c8b8ce89f2a909ec5309015e7b4673ef00
SamOstrander <samostr@gmail.com>
2023-09-30 11:57:09 -0500

    added talking.lua and some lines

---
+++ media/lua/server/talking.lua
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+------------------------------------------------------
+--separate first into puppet type, then into dialogue type
+
+-- madge --
+local madgeAttackList = {
+
+}
+
+local madgeHurtList = {
+
+}
+
+local madgeOtherList = {
+
+}
+
+local madgeDeathList = {
+    "Fuck this shit.",
+
+}
+
+
+
+-- sadge --
+
+local sadgeAttackList = {
+    "Oh bother...",
+    "Are we done with all this yet?",
+    "I want to go home.",
+    
+
+
+}
+
+local sadgeHurtList = {
+    "Please stop.",
+    "Owie.",
+    "It hurts :(.",
+
+
+}
+
+local sadgeOtherList = {
+    "I want to lay down.",
+}
+
+local sadgeDeathList = {
+    "Now I can rest.",
+    "Time for a deep sleep.",
+    "Finally.",
+
+
+}
+
+
+-- kookycrazy --
+
+
+local kookyAttackList = {
+    "WOOooOOoo",
+    "YAAAaaaY",
+
+}
+
+local kookyHurtList = {
+    "HAaAHAHAa",
+    "OuCHIE, AHAHAHA",
+
+}
+
+local kookyOtherList = {
+    
+    
+}
+
+local kookyDeathList = {
+    "HaaHAAa OW",
+
+}
