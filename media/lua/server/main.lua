4ef116baa1ffc93ec533240fe5895711c53be2c0
SamOstrander <samostr@gmail.com>
2023-10-19 14:40:45 -0500

    restructured player data storage

--- media/lua/server/main.lua
+++ media/lua/server/main.lua
@@ -7,13 +7,9 @@ objectData.State = OTHER
 
 local function initPlayerData(player)
     objectData[player] = {}
-    objectData[player].talkTimer 
+    objectData[player].talkTimer = Timer:new(ZombRandFloat(SandboxVars.SockPuppets.MinTalkTimer,SandboxVars.SockPuppets.MaxTalkTimer), false)
+    objectData[player].puppetId = nil
 end
-
---local function initZombieData(zombie)
---    objectData[zombie] = {}
---    objectData[zombie].talkTimer = Timer:new(ZombRandFloat(SandboxVars.SockPuppets.MinTalkTimer,SandboxVars.SockPuppets.MaxTalkTimer),false)
---end
 
 local function getRandomLine(lines)
     local count = 0
@@ -29,7 +25,28 @@ local function GetLinesFromState(puppetId, stateId)
     return lineDB.puppetId.stateId
 end
 
-local function OnPlayerUpdate(player)
+local function onClothingUpdated(playerOrCharacter)
+--if player and if puppet is equipped, set puppetId
+    local items = playerOrCharacter.getInventory():getItems()
+    for i=0, items:size() - 1 do
+        local item = items:get(i);
+        --check if item is a sockpuppet and item is equipped
+        if item:isEquipped() then
+            
+        end
 
+    end
+end
+
+local function OnPlayerUpdate(player)
+    if objectData[player] == nil then
+        initPlayerData(player)
+    end
+    local playerData = objectData[player]
+    local talkTime = playerData.talkTimer:tick()
+
+    if talkTime then
+        local line = getRandomLine(GetLinesFromState())
+    end
 end
 Events.onPlayerUpdate.Add(OnPlayerUpdate)
