14eec42daf9d4ad71144a630396e4037468d6dcd
SamOstrander <samostr@gmail.com>
2023-11-21 16:31:25 -0600

    bugfixes and other stuff

--- media/lua/server/main.lua
+++ media/lua/server/main.lua
@@ -6,14 +6,13 @@ require "names"
 ModuleName = "SockPuppets"
 
 objectData = {}
-objectData.State = OTHER
+--objectData.State = OTHER
 
 local function initPlayerData(player)
     objectData[player] = {}
         --talkTimer 1 is for other, timer2 would be a shorter timer for things like attacks and taking damage. 
     objectData[player].talkTimer = Timer:new(ZombRandFloat(SandboxVars.SockPuppets.MinTalkTimer*2,SandboxVars.SockPuppets.MaxTalkTimer*2), false)
     objectData[player].talkTimer2 = Timer:new(ZombRandFloat(SandboxVars.SockPuppets.MinTalkTimer,SandboxVars.SockPuppets.MaxTalkTimer), false)
-    
     objectData[player].puppetId = nil
 end
 
@@ -30,6 +29,16 @@ local function GetLinesFromState(puppetId, stateId)
     return lineDB.puppetId.stateId
 end
 
+    --use in a game start/load event. clothing update probably wont run, so the puppet wouldn't be active. 
+local function isPuppetEquipped(player)
+    local hands = player:getClothingItem_Hands()
+    if hands:getModule() == ModuleName then
+        --a puppet is equipped
+        objectData[player].puppetId = hands:getName()
+        print(objectData[player].puppetId)
+    end
+end
+
 local function OnClothingUpdated(playerOrCharacter) --there's a chance that this isn't great. if a character is a zombied than this would check every 
     --getClothingItem_Hands --maybe???
     --getWornItems      --contender
@@ -43,7 +52,9 @@ local function OnClothingUpdated(playerOrCharacter) --there's a chance that this
     --    end
     --end
 
-    if ~playerOrCharacter:playerIsSelf() then
+    playerOrCharacter:addLineChatElement("clothing updated")
+
+    if playerOrCharacter.isoPlayer == nil then
         return 0
     end
 
@@ -54,26 +65,38 @@ local function OnClothingUpdated(playerOrCharacter) --there's a chance that this
         objectData[playerOrCharacter].puppetId = hands:getName()
     end
 end
-Event.OnClothingUpdated.Add(OnClothingUpdated)
+Events.OnClothingUpdated.Add(OnClothingUpdated)
+
+test = false
 
 local function OnPlayerUpdate(player)
-    --if objectData[player] == nil then
-    --    initPlayerData(player)
-    --end
-    local playerData = objectData[player]
-    local talkTime = playerData.talkTimer:tick()
-
-    if talkTime and playerData.puppetId ~= nil then
-        local line = getRandomLine(GetLinesFromState(playerData.puppetId,OTHER))    --on player update would always be other? trigger for attack, hurt and death would be from other events.
-        player:addLineChatElement(line:getLine())
+    if objectData[player] == nil then
+        initPlayerData(player)
+        player:addLineChatElement("initplayerdata")
+        test = true
     end
-    --print line to screen here
-    --also trigger sound
+
+    if objectData[player].puppetId ~= nil then
+    
+        local playerData = objectData[player]
+        local talkTime = playerData.talkTimer:tick()
+
+        if talkTime then
+            if playerData.puppetId ~= nil then
+                local line = getRandomLine(GetLinesFromState(playerData.puppetId,OTHER))    --on player update would always be other? trigger for attack, hurt and death would be from other events.
+                player:addLineChatElement(line:getLine())
+                player:addLineChatElement("puppet line")
+            end
+        end
+        --trigger sound
+    end
+
 end
 Events.OnPlayerUpdate.Add(OnPlayerUpdate)
 
 
 local function OnPlayerDeath(player)
+    player:addLineChatElement("player death")
     --if objectData[player] == nil then
     --    initPlayerData(player)
     --end
@@ -81,6 +104,7 @@ local function OnPlayerDeath(player)
     if playerData.puppetId ~= nil then
         local line = getRandomLine(GetLinesFromState(playerData.puppetId,DEATH))    --on player update would always be other? trigger for attack, hurt and death would be from other events.
         player:addLineChatElement(line:getLine())
+        player:addLineChatElement("puppet line")
     end
     --print line to screen here
     --also trigger sound
@@ -89,7 +113,9 @@ Events.OnPlayerDeath.Add(OnPlayerDeath)
 
 --check if onweaponhitcharacter triggers on zombies hitting player
     --OnHitZombie works, probably better than 
-local function OnWeaponHitXP(player, handWeapon, character, damageSplit)
+local function OnWeaponHitXp(player, handWeapon, character, damageSplit)
+
+    player:addLineChatElement("weapon hit xp")
 
     local playerData = objectData[player]
     local talkTime = playerData.talkTimer2:tick()
@@ -97,24 +123,31 @@ local function OnWeaponHitXP(player, handWeapon, character, damageSplit)
     if talkTime and playerData.puppetId ~= nil then
         local line = getRandomLine(GetLinesFromState(playerData.puppetId,ATTACK))    --on player update would always be other? trigger for attack, hurt and death would be from other events.
         player:addLineChatElement(line:getLine())
+        player:addLineChatElement("puppet line")
     end
     --print line to screen here
     --also trigger sound
 end
-Events.OnWeaponHitXP.Add(OnWeaponHitXP)
+Events.OnWeaponHitXp.Add(OnWeaponHitXp)
 --OnPlayerGetDamage
 --local function OnWeaponHitCharacter(wielder, character, handWeapon, damage)
 local function OnPlayerGetDamage(player, damageType, damage)
+    if damageType ~= "WEAPONHIT" then
+        player:addLineChatElement("weaponhit")
+    end
+
+    --player:addLineChatElement("player get damage")
     if objectData[player] == nil or (damageType ~= "WEAPONHIT" and damageType ~= "FALLDOWN" and damageType ~= "FIRE" and damageType ~= "CARHITDAMAGE" and damageType ~= "CARCRASHDAMAGE")  then --specifies damage types that can trigger the puppet talking
         return 0
     end
-
+    player:addLineChatElement("playerhit")
     local playerData = objectData[player]
     local talkTime = playerData.talkTimer2:tick()
 
     if talkTime and playerData.puppetId ~= nil then
         local line = getRandomLine(GetLinesFromState(playerData.puppetId,HURT))    --on player update would always be other? trigger for attack, hurt and death would be from other events.
         player:addLineChatElement(line:getLine())
+        player:addLineChatElement("puppet line")
     end
 end
 
--- media/lua/server/talking.lua
+++ media/lua/server/talking.lua
@@ -1,4 +1,19 @@
-require "enums"
+--require "enums"
+
+--
+-- constants for table indices
+
+--puppet ids
+SADGE =1
+MADGE =2
+KOOKY =3
+
+--state ids
+ATTACK =1
+HURT =2
+OTHER =3
+DEATH =4
+--
 
 lineDB = {}
 lineDB.SADGE = {}
